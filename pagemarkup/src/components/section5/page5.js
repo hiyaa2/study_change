969dcf826f419e80a8a689600693a1484f412bdd
hiyaa2 <hiya9132@naver.com>
2022-06-17 18:35:50 +0900

    pagemarkup0617sec

--- pagemarkup/src/components/section5/page5.js
+++ pagemarkup/src/components/section5/page5.js
@@ -6,15 +6,39 @@ function Section5() {
   // const arr = [document.getElementById("profile0"),document.getElementById("profile1"),document.getElementById("profile2"),document.getElementById("profile3"),document.getElementById("profile4"),document.getElementById("profile5")];
   const handleClickImage1 = () => {
     document.getElementById("slideshow-profile").style.left = '240px';
+    document.getElementById("profilesize1").style.width = '170px';
+    document.getElementById("profilesize1").style.height = '170px';
+    document.getElementById("profilesize2").style.width = '170px';
+    document.getElementById("profilesize2").style.height = '170px';
+    document.getElementById("profilesize3").style.width = '170px';
+    document.getElementById("profilesize3").style.height = '170px';
     };
   const handleClickImage2 = () => {
     document.getElementById("slideshow-profile").style.left = 0;
+    document.getElementById("profilesize1").style.width = '280px';
+    document.getElementById("profilesize1").style.height = '280px';
+    document.getElementById("profilesize2").style.width = '170px';
+    document.getElementById("profilesize2").style.height = '170px';
+    document.getElementById("profilesize3").style.width = '170px';
+    document.getElementById("profilesize3").style.height = '170px';
   };
   const handleClickImage3 = () => {
     document.getElementById("slideshow-profile").style.left = '-240px';
+    document.getElementById("profilesize2").style.width = '280px';
+    document.getElementById("profilesize2").style.height = '280px';
+    document.getElementById("profilesize1").style.width = '170px';
+    document.getElementById("profilesize1").style.height = '170px';
+    document.getElementById("profilesize3").style.width = '170px';
+    document.getElementById("profilesize3").style.height = '170px';
   };
   const handleClickImage4 = () => {
     document.getElementById("slideshow-profile").style.left = '-480px';
+    document.getElementById("profilesize3").style.width = '280px';
+    document.getElementById("profilesize3").style.height = '280px';
+    document.getElementById("profilesize1").style.width = '170px';
+    document.getElementById("profilesize1").style.height = '170px';
+    document.getElementById("profilesize2").style.width = '170px';
+    document.getElementById("profilesize2").style.height = '170px';
   }; 
   return(
   <div className="page5-wrapper">
@@ -25,21 +49,21 @@ function Section5() {
             <div className="page5image1"></div>
           </div>
           <div id ="profile1" className="page5active">
-            <div className="page5image2"></div>
+            <div id = "profilesize1"className="page5image2"></div>
             <div>
               <div className="namecolor">Elisabeth Be</div>
               <div className="jobcolor">COO</div>
             </div>
           </div>
           <div id ="profile2" className="page5active">
-            <div className="page5image3"></div>
+            <div id = "profilesize2" className="page5image3"></div>
             <div>
               <div className="namecolor">Andree Wijaya</div>
               <div className="jobcolor">CEO/CTO</div>
             </div>
           </div>
           <div id ="profile3" className="page5active">
-            <div className="page5image4"></div>
+            <div id = "profilesize3" className="page5image4"></div>
             <div>
               <div className="namecolor">Sanjaya Wahuiu</div>
               <div className="jobcolor">CFO</div>
